000eeab26aef11e7dfe513e60d3b8d4770b74645
Sebastian Acosta <mu9seb@gmail.com>
2023-11-25 13:38:22 -0700

    WB5 directory merged

--- workbook3/functions/f_to_c.js
+++
@@ -1,5 +0,0 @@
-function convertFtoC(temp_f) {
-    let temp_c = (temp_f-32) * (5/9);
-    let displayMessage = console.log(`A temperature of ${temp_f} degrees Fahrenheit equates to ${temp_c} degrees Celcius.`)
-    return displayMessage;
-}
